af7c549a6e6bc6e943349d34531e9dd4ac35f0e3
melinox58 <melinox89@yahoo.fr>
2025-01-27 19:07:35 +0100

    secret

--- Livrables/DataBase/CreateNoSQL.js
+++
@@ -1,4 +0,0 @@
-db.images.insertMany([
-    { filename: "image1", path: "public/uploads/products/image1.webp", alt_text: "T-shirt blanc"},
-    { filename: "image2", path: "public/uploads/products/image2.webp", alt_text: "Jean slim" },
-]);
